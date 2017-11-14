88519732086e979fecbb0fbc3494a3075d4f2f05
crystal-chenjie <1468401217@qq.com>
2017-11-14 10:33:03 +0800

    Update and rename 01 to 01-创建按钮.js

---
+++ 01-创建按钮.js
@@ -0,0 +1,16 @@
+
+// 创建按钮
+
+      var mingLouBtn:starlingswf.SwfButton = this.createOpBtn(9,0);
+			this.opEventList[this.opEventNo] = [mingLouBtn,9,tings];
+			this.opEventNo++;
+
+//  mingLouBtn  是实例化出的 btn 调用创建 button 的 函数
+//  9 是 根据上面协议好的数字 表示的操作
+//  tings 表示该数字表示的操作 所携带的数据
+
+// this.opEventList[this.opEventNo] 表示后端接口 后面表示前端发过去的数据
+
+
+
+  
