d1ffe0a996f9c69ba3e8bcbd5c6dfe3efba490dd
crystal-chenjie <1468401217@qq.com>
2017-11-14 10:33:35 +0800

    Update 01-创建按钮.js

--- 01-创建按钮.js
+++ 01-创建按钮.js
@@ -1,9 +1,9 @@
 
 // 创建按钮
 
-      var mingLouBtn:starlingswf.SwfButton = this.createOpBtn(9,0);
-			this.opEventList[this.opEventNo] = [mingLouBtn,9,tings];
-			this.opEventNo++;
+        var mingLouBtn:starlingswf.SwfButton = this.createOpBtn(9,0);
+	this.opEventList[this.opEventNo] = [mingLouBtn,9,tings];
+	this.opEventNo++;
 
 //  mingLouBtn  是实例化出的 btn 调用创建 button 的 函数
 //  9 是 根据上面协议好的数字 表示的操作
